37cef697edb8ea757b1a88d9ce4443f1587abf0b
Nugroho Dwi Riyanto <nugroho1116@gmail.com>
2025-11-15 15:39:50 +0700

    Update gallery.blade.php

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -159,7 +159,7 @@
                         @endforeach
                     </div>
 
-                    @if($testimoni->isEmpty() && $produk->isEmpty())
+                    @if($testimoni->isEmpty() && $foto>isEmpty())
                     <div class="row">
                         <div class="col-12 text-center">
                             <div class="alert alert-info">
